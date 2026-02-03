53ae0242041f7abbecb4c908c27fd04fc03a2703
Grishma Acharya <acharyagrishma73@gmail.com>
2026-02-03 15:37:20 +0545

    Test Case Scenario

---
+++ Features/Steps/StepDefination.py
@@ -0,0 +1,71 @@
+from behave import *
+from selenium.webdriver import Chrome
+from behave.api.pending_step import StepNotImplementedError
+
+
+@given(u'User is on Registration Page')
+def step_imp(context):
+    context.driver=Chrome()
+    context.driver.get('https://www.facebook.com/r.php')
+    context.driver.maximize_window()
+
+@when(u'User enters firstname')
+def step_imp(context):
+    context.driver.find_element('name', 'firstname').send_keys("riya")
+
+@when(u'User enters lastname')
+def step_imp(context):
+    #raise NotImplementedError(u'STEP:User enters lastname')
+    context.driver.find_element('name', 'lastname').send_keys("kumARI")
+
+@when(u'User enters month')
+def step_imp(context):
+    context.driver.find_element('xpath', '//select[@name="birthday_month"]').send_keys("Feb")
+
+@when(u'User enters day')
+def step_imp(context):
+     context.driver.find_element('xpath', '//select[@name="birthday_day"]').send_keys("12")
+
+@when(u'User enters year')
+def step_imp(context):
+     context.driver.find_element('xpath', '//select[@name="birthday_year"]').send_keys("2005")
+
+@when(u'User click gender')
+def step_imp(context):
+     context.driver.find_element('xpath', '//input[@value="2"]').click()
+
+@when(u'User enters email')
+def step_imp(context):
+    context.driver.find_element('xpath', '//input[@name="reg_email__"]').send_keys("abc@gmail.com")
+
+@when(u'User enters password')
+def step_imp(context):
+     context.driver.find_element('xpath', '//input[@name="reg_passwd__"]').send_keys("12345")
+
+@when(u'User click on signup button')
+def step_imp(context):
+   context.driver.find_element('xpath', '//button[@name="websubmit"]').click()
+
+@then(u'User should be registered successfully')
+def step_imp(context):
+    print("Registered")
+
+@when(u'User enters duplicate email')
+def step_imp(context):
+    raise StepNotImplementedError('User enters duplicate email')
+
+@then(u'User should be registered with duplicate email successfully')
+def step_impl(context):
+    raise StepNotImplementedError(u'Then User should be registered  with duplicate email successfully')   
+
+
+    
+
+
+
+
+
+
+
+
+
